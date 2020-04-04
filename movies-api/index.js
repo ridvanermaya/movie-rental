8ffe5bc89381e2a5f978559b4d6f75f918feb9e9
Ridvan Ermaya <ridvanermaya@gmail.com>
2020-04-04 13:01:31 -0500

    express installed, started a dumbby api endpoint

---
+++ movies-api/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/api', (req, res, next) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on port: http://localhost:${port}`);
+});
